7af0f186eab29a7b85655d114f13326de30bf889
ntnyq <ntnyq13@gmail.com>
2023-09-03 05:51:41 +0800

    test: test with mock env

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,25 @@
-import { expect, it } from 'vitest'
+import { isCI } from 'std-env'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { isInEditor } from '../src'
 
-it('should not in editor', () => {
-  expect(isInEditor()).toBeFalsy()
+describe('is-in-editor', () => {
+  beforeEach(() => {
+    vi.unstubAllEnvs()
+  })
+
+  it('should not pass when in CI', () => {
+    expect(isInEditor()).toBeFalsy()
+  })
+
+  it.skipIf(isCI)('should pass when env VSCODE_PID is set', () => {
+    vi.stubEnv('VSCODE_PID', '1')
+
+    expect(isInEditor()).toBeTruthy()
+  })
+
+  it.skipIf(isCI)('should pass when env JETBRAINS_IDE is set', () => {
+    vi.stubEnv('JETBRAINS_IDE', '1')
+
+    expect(isInEditor()).toBeTruthy()
+  })
 })
